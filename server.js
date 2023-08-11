7919614fbaee0a1d5d2a3891fd73df03593a24b2
BenWhann <whann004@umn.edu>
2023-08-10 21:33:36 -0500

    first route attempt

--- server.js
+++ server.js
@@ -1,16 +1,74 @@
-const express = require("express");
-const expbs = require("express-handlebars");
-const hbs = expbs.create();
+// const express = require("express");
+// const expbs = require("express-handlebars");
+// const hbs = expbs.create();
+// const app = express();
+// const path = require('path');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(require('./controllers'));
+
+// app.engine("handlebars", hbs.engine);
+
+// app.set("view engine", "handlebars");
+
+// app.get("/", (req, res) => {
+//   res.render("homepage");
+// });
+
+// app.get("/login", (req, res) => {
+//   res.render("login");
+// })
+
+// app.listen(3001, () => {
+//   console.log("Listening on port 3001");
+// });
+
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.engine("handlebars", hbs.engine);
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
-app.set("view engine", "handlebars");
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-app.get("/", (req, res) => {
-  res.render("homepage");
-});
+app.use(session(sess));
 
-app.listen(3001, () => {
-  console.log("Listening on port 3001");
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
