2bbeea7ed4d6bd7acd4a5509718aaae0da9ee237
BenWhann <whann004@umn.edu>
2023-08-11 17:18:16 -0500

    first route saves to db

--- public/js/login.js
+++ public/js/login.js
@@ -18,7 +18,7 @@ const loginFormHandler = async (event) => {
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/');
+        document.location.replace('/'); // re-direct elsewhere
       } else {
         alert(response.statusText);
       }
